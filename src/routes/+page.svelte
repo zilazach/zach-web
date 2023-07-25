f32fbb529eab9e9c74d9d422dbf07e589c5d3096
zilazach <105255994+zilazach@users.noreply.github.com>
2023-07-26 11:27:00 +1200

    add pratice code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h2>html practice</h2>
+<ul>
+    <li>cat</li>
+    <li>dog</li>
+    <li>fish</li>
+    <li>bird</li>
+</ul>
+<div>
+    <div>
+        <img src="https://moodle.op.ac.nz/pluginfile.php/1537023/mod_page/content/36/html%20practice.JPG" width="300"/>
+    </div>
+    <div>
+        <a href="https://online.op.ac.nz/">otago polytechnic</a> 
+    </div>
+</div>
+
+
+<!--<img srv="./images/dinosaurs_img-trex.jpg"  width="300" />-->
