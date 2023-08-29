75d82b016eff24e834f90c211fb8f43991d76484
zilazach <105255994+zilazach@users.noreply.github.com>
2023-08-30 09:44:32 +1200

    add the start to responsiveness

--- src/routes/page2/+page.svelte
+++ src/routes/page2/+page.svelte
@@ -1,1 +1,51 @@
-page 2
+<style>
+
+    body{
+        padding:0 100px;
+    }
+
+    .col_3{
+        display:grid;
+        grid-template-columns:repeat(3, 1fr);
+        gap:30px;
+    }
+
+    .col_3 div{
+        height:300px;
+        background:lightblue;
+        display:flex;
+        align-items: center;
+        justify-content:center;
+    }
+
+    @media (max-width:800px){
+        .col_3{
+            grid-template-columns: repeat(1,1fr);
+            
+        }
+        
+        
+    }
+
+</style>
+
+<div class="col_3">
+    <div>
+        <h2>Products</h2>
+    </div>
+    <div>
+        <h2>Products</h2>
+    </div>
+    <div>
+        <h2>Products</h2>
+    </div>
+    <div>
+        <h2>Products</h2>
+    </div>
+    <div>
+        <h2>Products</h2>
+    </div>
+    <div>
+        <h2>Products</h2>
+    </div>
+</div>
