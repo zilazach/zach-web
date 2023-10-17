431ad49ea1ca4b814fbfc59007f7f193f93aca34
zilazach <105255994+zilazach@users.noreply.github.com>
2023-10-18 10:04:28 +1300

    add api

---
+++ src/routes/page3/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+    import { onMount } from 'svelte';
+
+    let imgs = [];
+    const BASE_URL = `https://api.unsplash.com`;
+    onMount(() => {
+        fetch(`${BASE_URL}/search/photos?per_page=2&query=dogs and cats&client_id=KLCfUHu8K1y3D8W_XILJ0rSKCdlCGvud5075PT3x-Yk`)
+        .then(res => res.json())
+        .then(data => {
+            imgs = data.results;
+        });
+    });
+</script>
+
+
+{#each imgs as img}
+    <img src= "{img.urls.regular}" alt="">
+{/each}
