da16e0089c7ac9e4c0d7752d50abd75ae3869e6a
zilazach <105255994+zilazach@users.noreply.github.com>
2023-10-27 11:43:13 +1300

    fix img and header

--- src/routes/page3/+page.svelte
+++ src/routes/page3/+page.svelte
@@ -15,9 +15,9 @@
 
 <!--images from the api and the img folder-->
 {#each imgs as img}
-    <img src= "{img.urls.regular}" alt="not a dino" style = "width:500px;height:350px">
+    <img src= "{img.urls.regular}" alt="not a dino" class="page3img">
 {/each}
 <br>
-<img src= "img\fate-pets.webp" alt = "not a dino" style = "width:500px;height:350px"> 
-<img src= "img\fate-pets-2.jpg" alt = "not a dino" style = "width:500px;height:350px"> 
+<img src= "img\fate-pets.webp" alt = "not a dino" class="page3img"> 
+<img src= "img\fate-pets-2.jpg" alt = "not a dino" class="page3img"> 
 <img src="img\dogs&cats.jpg" alt="not a dino">
