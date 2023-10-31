417eb2f5788f7c9b06fe03c3421794f307f66b9b
zilazach <105255994+zilazach@users.noreply.github.com>
2023-11-01 11:22:41 +1300

    work on api and nav img

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,6 +1,6 @@
 <!--The navigation for the site-->
 <nav>
-    <a href="/">Home</a>
+   <a href="/">Home</a>
     <a href="/page2">page2</a>
     <a href="/page3">page3</a>
 </nav>
--- src/routes/page3/+page.svelte
+++ src/routes/page3/+page.svelte
@@ -5,12 +5,19 @@
     let imgs = [];
     const BASE_URL = `https://api.unsplash.com`;
     onMount(() => {
-        fetch(`${BASE_URL}/search/photos?per_page=2&query=dogs and cats&client_id=KLCfUHu8K1y3D8W_XILJ0rSKCdlCGvud5075PT3x-Yk`)
+        //async()=>{
+        //    res = await fetch(`${BASE_URL}/search/photos?per_page=2&query=dogs with cats&client_id=KLCfUHu8K1y3D8W_XILJ0rSKCdlCGvud5075PT3x-Yk`)
+        //    data = await res.json()
+        //    imgs = data.results
+        //}
+        
+        fetch(`${BASE_URL}/search/photos?per_page=2&query=dogs with cats&client_id=KLCfUHu8K1y3D8W_XILJ0rSKCdlCGvud5075PT3x-Yk`)
         .then(res => res.json())
         .then(data => {
             imgs = data.results;
+
         });
-    });
+    }, 1);
 </script>
 
 <!--images from the api and the img folder-->
