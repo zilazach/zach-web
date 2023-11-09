b44ed1fde63c9341db53eb1a9cbc75e08016960b
zilazach <105255994+zilazach@users.noreply.github.com>
2023-11-10 11:06:02 +1300

    fix the nav imgs and page3 imgs

--- src/routes/page2/+page.svelte
+++ src/routes/page2/+page.svelte
@@ -1,7 +1,7 @@
 <!--image and text-->
 <div class="flex_parent">
     <div class="child child1"><img src="img\Iori.webp" alt="not a dino"></div>
-    <div class="child child2">wordswordswordswordswordswordswordswordswordswordswordswordswordswordswordswordswordswordswordswordswords</div>
+    <div class="child child2">Miyamoto Iori Sadatsugu is the student and adopted son of Miyamoto Musashi. He is a ronin living in Asakusa, Edo, who studied the Niten Ichiryu style of swordsmanship, but Musashi passed away before he could teach him the secrets of the art. Still seeking to master the Way of the sword, he spends his days training while making ends meet.</div>
   </div>
 
   <div class="flex_parent">
--- src/routes/page3/+page.svelte
+++ src/routes/page3/+page.svelte
@@ -21,11 +21,15 @@
 </script>
 <div class="contents">
 <!--images from the api and the img folder-->
-{#each imgs as img}
+<div>
+    {#each imgs as img}
     <img src= "{img.urls.regular}" alt="not a dino" class="page3img">
-{/each}
+    {/each}
+</div>
 <br>
-<img src= "img\fate-pets.webp" alt = "not a dino" class="page3img"> 
-<img src= "img\fate-pets-2.jpg" alt = "not a dino" class="page3img"> 
+<div>   
+    <img src= "img\fate-pets.webp" alt = "not a dino" class="page3img"> 
+    <img src= "img\fate-pets-2.jpg" alt = "not a dino" class="page3img"> 
+</div>
 <img src="img\dogs&cats.jpg" alt="not a dino">
 </div>
